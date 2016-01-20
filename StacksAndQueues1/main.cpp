4fe590e0bdeb6a3d16f10b457d878579581f4956
kwkevinlin <kwkevinlin@gmail.com>
2016-01-19 22:18:32 -0500

    Added Question 1 in .txt

---
+++ StacksAndQueues1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main () {
+
+	/*
+	 * Question: Describe how you could use a single array to implement three stacks.
+	 *
+	 * Solution: Traverse the linked list from the beginning, and use int i to track which index
+	 * 			 you are on. If i = index (which is size() - k), cout, then break. Else, i++
+	 *
+	 * Complexity: O(n) for the one loop
+	 *
+	 */
+
+}
